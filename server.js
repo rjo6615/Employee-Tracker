873083d1617bbb5441cfabfe4105cd4b0366d478
rjo6615 <robertjobernier@gmail.com>
2022-12-05 13:43:26 -0500

    added inquirer prompt

--- server.js
+++ server.js
@@ -17,8 +17,54 @@ connection.connect(err => {
 afterConnect = () => {
     console.log("***********************************")
     console.log("*                                 *")
-    console.log("*        EMPLOYEE Tracker         *")
+    console.log("*        EMPLOYEE TRACKER         *")
     console.log("*                                 *")
     console.log("***********************************")
-    // promptUser();
+    Startprompt();
   };
+  const Startprompt = () => {
+    inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'choices', 
+        message: 'What would you like to do?',
+        choices: ['View All Employees', 
+                  'Add Employee',
+                  'Update Employee Role',
+                  'View All Roles',
+                  'Add Role',
+                  'View All Departments',
+                  'Add Department',
+                  'Exit'
+                ]
+      }
+    ])
+      .then((answers) => {
+        const { choices } = answers; 
+        if (choices === "View All Employees") {
+            showEmployees();
+          }
+        if (choices === "Add Employee") {
+            addEmployee();
+          }
+        if (choices === "Update Employee Role") {
+            updateEmployee();
+          }
+        if (choices === "View All Roles") {
+            showRoles();
+          }
+        if (choices === "Add Role") {
+            addRole();
+          }  
+        if (choices === "View All Departments") {
+          showDepartments();
+        }  
+        if (choices === "Add Department") {
+          addDepartment();
+        }
+        if (choices === "Exit") {
+          connection.end()
+      };
+    });
+  };
+  
